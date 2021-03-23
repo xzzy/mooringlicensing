7c1cc8e8a23b6ac56587e3be1986ca69b3101c02
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-03-23 09:22:55 +0800

    Fix process_document for electoral_roll_documents

--- mooringlicensing/components/main/process_document.py
+++ mooringlicensing/components/main/process_document.py
@@ -11,7 +11,6 @@ from mooringlicensing.components.proposals.models import (
 
 
 def process_generic_document(request, instance, document_type=None, *args, **kwargs):
-    import ipdb; ipdb.set_trace()
     print("process_generic_document")
     print(request.data)
     try:
@@ -44,16 +43,8 @@ def process_generic_document(request, instance, document_type=None, *args, **kwa
         elif input_name:
             if document_type == 'electoral_roll_document':
                 documents_qs = instance.electoral_roll_documents
-        #elif input_name:
-        #    if document_type == DeedPollDocument.DOC_TYPE_NAME:
-        #        documents_qs = instance.deed_poll_documents
-        #    elif document_type == PublicLiabilityInsuranceDocument.DOC_TYPE_NAME:
-        #        documents_qs = instance.public_liability_insurance_documents
-        #    elif document_type == SupportingApplicationDocument.DOC_TYPE_NAME:
-        #        documents_qs = instance.supporting_application_documents
-
-        #    returned_file_data = [dict(file=d._file.url, id=d.id, name=d.name,) for d in documents_qs.filter(input_name=input_name) if d._file]
-        #    return { 'filedata': returned_file_data }
+            returned_file_data = [dict(file=d._file.url, id=d.id, name=d.name,) for d in documents_qs.filter(input_name=input_name) if d._file]
+            return { 'filedata': returned_file_data }
         else:
             returned_file_data = [dict(file=d._file.url, id=d.id, name=d.name, ) for d in instance.documents.all() if d._file]
             return {'filedata': returned_file_data}
@@ -68,6 +59,7 @@ def delete_document(request, instance, comms_instance, document_type, input_name
     if 'document_id' in request.data:
         if document_type == 'electoral_roll_document':
             document_id = request.data.get('document_id')
+            document = instance.electoral_roll_documents.get(id=document_id)
         #if document_type == DeedPollDocument.DOC_TYPE_NAME:
         #    document_id = request.data.get('document_id')
         #    document = instance.deed_poll_documents.get(id=document_id)
@@ -120,58 +112,58 @@ def cancel_document(request, instance, comms_instance, document_type, input_name
 
 
 def save_document(request, instance, comms_instance, document_type, input_name=None):
-        # Match model related_name to instance or comms_instance, eg.
-        # sanction_outcome = models.ForeignKey(SanctionOutcome, related_name='documents')..
-        # this document can be accessed or created by 'instance.documents'
+    # Match model related_name to instance or comms_instance, eg.
+    # sanction_outcome = models.ForeignKey(SanctionOutcome, related_name='documents')..
+    # this document can be accessed or created by 'instance.documents'
 
-        # example document_type
-        if 'filename' in request.data and input_name:
-            filename = request.data.get('filename')
-            _file = request.data.get('_file')
+    # example document_type
+    if 'filename' in request.data and input_name:
+        filename = request.data.get('filename')
+        _file = request.data.get('_file')
 
-            if document_type == 'electoral_roll_document':
-                document = instance.electoral_roll_documents.get_or_create(input_name=input_name, name=filename)[0]
-                path_format_string = '{}/emailuser/{}/electoral_roll_documents/{}'
-            #if document_type == DeedPollDocument.DOC_TYPE_NAME:
-            #    document = instance.deed_poll_documents.get_or_create(input_name=input_name, name=filename)[0]
-            #    path_format_string = '{}/proposals/{}/deed_poll_documents/{}'
-            #elif document_type == PublicLiabilityInsuranceDocument.DOC_TYPE_NAME:
-            #    document = instance.public_liability_insurance_documents.get_or_create(input_name=input_name, name=filename)[0]
-            #    path_format_string = '{}/proposals/{}/public_liability_insurance_documents/{}'
-            #elif document_type == SupportingApplicationDocument.DOC_TYPE_NAME:
-            #    document = instance.supporting_application_documents.get_or_create(input_name=input_name, name=filename)[0]
-            #    path_format_string = '{}/proposals/{}/supporting_application_documents/{}'
-            path = default_storage.save(path_format_string.format(settings.MEDIA_APP_DIR, instance.id, filename), ContentFile(_file.read()))
-            document._file = path
-            document.save()
+        if document_type == 'electoral_roll_document':
+            document = instance.electoral_roll_documents.get_or_create(input_name=input_name, name=filename)[0]
+            path_format_string = '{}/emailuser/{}/electoral_roll_documents/{}'
+        #if document_type == DeedPollDocument.DOC_TYPE_NAME:
+        #    document = instance.deed_poll_documents.get_or_create(input_name=input_name, name=filename)[0]
+        #    path_format_string = '{}/proposals/{}/deed_poll_documents/{}'
+        #elif document_type == PublicLiabilityInsuranceDocument.DOC_TYPE_NAME:
+        #    document = instance.public_liability_insurance_documents.get_or_create(input_name=input_name, name=filename)[0]
+        #    path_format_string = '{}/proposals/{}/public_liability_insurance_documents/{}'
+        #elif document_type == SupportingApplicationDocument.DOC_TYPE_NAME:
+        #    document = instance.supporting_application_documents.get_or_create(input_name=input_name, name=filename)[0]
+        #    path_format_string = '{}/proposals/{}/supporting_application_documents/{}'
+        path = default_storage.save(path_format_string.format(settings.MEDIA_APP_DIR, instance.id, filename), ContentFile(_file.read()))
+        document._file = path
+        document.save()
 
-        # comms_log doc store save
-        elif comms_instance and 'filename' in request.data:
-            filename = request.data.get('filename')
-            _file = request.data.get('_file')
+    # comms_log doc store save
+    elif comms_instance and 'filename' in request.data:
+        filename = request.data.get('filename')
+        _file = request.data.get('_file')
 
-            document = comms_instance.documents.get_or_create(
-                name=filename)[0]
-            path = default_storage.save(
-                '{}/{}/communications/{}/documents/{}'.format(
-                    instance._meta.model_name, instance.id, comms_instance.id, filename), ContentFile(
-                    _file.read()))
+        document = comms_instance.documents.get_or_create(
+            name=filename)[0]
+        path = default_storage.save(
+            '{}/{}/communications/{}/documents/{}'.format(
+                instance._meta.model_name, instance.id, comms_instance.id, filename), ContentFile(
+                _file.read()))
 
-            document._file = path
-            document.save()
+        document._file = path
+        document.save()
 
-        # default doc store save
-        elif 'filename' in request.data:
-            filename = request.data.get('filename')
-            _file = request.data.get('_file')
+    # default doc store save
+    elif 'filename' in request.data:
+        filename = request.data.get('filename')
+        _file = request.data.get('_file')
 
-            document = instance.documents.get_or_create(
-                name=filename)[0]
-            path = default_storage.save(
-                '{}/{}/documents/{}'.format(
-                    instance._meta.model_name, instance.id, filename), ContentFile(
-                    _file.read()))
+        document = instance.documents.get_or_create(
+            name=filename)[0]
+        path = default_storage.save(
+            '{}/{}/documents/{}'.format(
+                instance._meta.model_name, instance.id, filename), ContentFile(
+                _file.read()))
 
-            document._file = path
-            document.save()
+        document._file = path
+        document.save()
 
