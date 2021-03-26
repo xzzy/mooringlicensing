c76f83fa06313c8249d01bd1d546af56fdead5d5
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-03-26 09:11:37 +0800

    Fix a bug where missing the module name, ledger

--- mooringlicensing/components/payments/utils.py
+++ mooringlicensing/components/payments/utils.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-from checkout.utils import create_basket_session, create_checkout_session
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from ledger.checkout.utils import create_basket_session, create_checkout_session
 
 from mooringlicensing import settings
 
