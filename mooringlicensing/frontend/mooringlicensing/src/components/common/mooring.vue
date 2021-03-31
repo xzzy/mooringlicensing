135799845f17fe92cb1ee06366c299d35b31e88b
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-03-31 16:56:28 +0800

    Begin to add mooring component

---
+++ mooringlicensing/frontend/mooringlicensing/src/components/common/mooring.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <FormSection label="Preferred mooring area" Index="preferred_mooring_area">
+        <div class="row form-group">
+            <label for="" class="col-sm-3 control-label">Select one preferred mooring area.</label>
+        </div>
+        <div class="row form-group">
+            <div class="col-sm-2" v-for="mooring in moorings">
+                {{ mooring }}
+            </div>
+        </div>
+    </FormSection>
+</template>
+
+<script>
+import FormSection from '@/components/forms/section_toggle.vue'
+    export default {
+        name:'mooring',
+        components:{
+            FormSection,
+        },
+        props:{
+            proposal:{
+                type: Object,
+                required:true
+            }
+        },
+        data:function () {
+            return {
+                moorings: [
+                    'catherine',
+                    'longreach',
+                    'narrow',
+                    'stark',
+                    'geordie',
+                    'majorie',
+                    'porpoise',
+                    'thomson'
+                ],
+            }
+        },
+        computed: {
+        },
+        methods:{
+        },
+        mounted:function () {
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+</style>
+
