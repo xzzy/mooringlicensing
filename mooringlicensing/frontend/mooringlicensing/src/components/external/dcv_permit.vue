2514739c9c50cf3246985af785bcf8fee124a731
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-04-12 16:05:04 +0800

    Add DCV pages

---
+++ mooringlicensing/frontend/mooringlicensing/src/components/external/dcv_permit.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container" id="externalDash">
+        DCV permit
+    </div>
+</template>
+
+<script>
+import datatable from '@/utils/vue/datatable.vue'
+import FormSection from "@/components/forms/section_toggle.vue"
+import { api_endpoints, helpers } from '@/utils/hooks'
+
+export default {
+    name: 'DcvTablePage',
+    data() {
+        let vm = this;
+        return {
+            empty_list: '/api/empty_list',
+            //proposals_url: helpers.add_endpoint_json(api_endpoints.proposals,'user_list'),
+            //approvals_url: helpers.add_endpoint_json(api_endpoints.approvals,'user_list'),
+            //compliances_url: helpers.add_endpoint_json(api_endpoints.compliances,'user_list'),
+
+            proposals_url: api_endpoints.proposals_paginated_external,
+            approvals_url: api_endpoints.approvals_paginated_external,
+            compliances_url: api_endpoints.compliances_paginated_external,
+
+            system_name: api_endpoints.system_name,
+        }
+    },
+    components:{
+        FormSection,
+    },
+    watch: {
+
+    },
+    computed: {
+        is_external: function() {
+            return this.level == 'external'
+        },
+
+    },
+    methods: {
+
+    },
+    mounted: function () {
+
+    },
+    created: function() {
+
+    },
+}
+</script>
