ab515214e28c3dc029c65c3966e6b29cdec10e0f
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-05-06 17:11:37 +0800

    Add internal dashboard pages. (Not completed)

--- mooringlicensing/frontend/mooringlicensing/src/components/internal/dcv/dashboard.vue
+++ mooringlicensing/frontend/mooringlicensing/src/components/internal/dcv/dashboard.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="container" id="dcvDash">
         <FormSection :formCollapse="false" label="DCV Permits" Index="dcv_permits">
-
+            <DcvPermitsTable
+                level="internal"
+            />
         </FormSection>
         <FormSection :formCollapse="false" label="DCV Admission Fees" Index="dcv_admission_fees">
-
+            <DcvAdmissionsTable
+                level="internal"
+            />
         </FormSection>
     </div>
 </template>
 
 <script>
 import FormSection from "@/components/forms/section_toggle.vue"
+import DcvPermitsTable from "@/components/common/table_dcv_permits.vue"
+import DcvAdmissionsTable from "@/components/common/table_dcv_admissions.vue"
+
 export default {
     name: 'InternalDcvDashboard',
     data() {
@@ -21,6 +28,8 @@ export default {
     },
     components:{
         FormSection,
+        DcvPermitsTable,
+        DcvAdmissionsTable,
     },
     methods: {
 
