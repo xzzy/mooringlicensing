8d47c3eb2984326ad20480be95506c86f70f90a9
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-05-27 14:18:00 +0800

    Add DcvAdmissions dash backend

--- mooringlicensing/frontend/mooringlicensing/src/api.js
+++ mooringlicensing/frontend/mooringlicensing/src/api.js
@@ -34,6 +34,7 @@ module.exports = {
     proposals_paginated_list: '/api/proposals_paginated', // both for external and internal
     approvals_paginated_list: '/api/approvals_paginated',
     dcvpermits_paginated_list: '/api/dcvpermits_paginated',
+    dcvadmissions_paginated_list: '/api/dcvadmissions_paginated',
     compliances_paginated_external: '/api/compliances_paginated/list_external',
     moorings_paginated_internal: '/api/moorings_paginated/list_internal',
     compliances:"/api/compliances.json",
