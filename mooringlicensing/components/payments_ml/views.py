c97610055ba49e4dbc903ea4a8b8226afbdb3d5e
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-06-01 10:24:19 +0800

    Fix a bug

--- mooringlicensing/components/payments_ml/views.py
+++ mooringlicensing/components/payments_ml/views.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import pytz
 from ledger.settings_base import TIME_ZONE
+from ledger.payments.pdf import create_invoice_pdf_bytes
 
 import dateutil.parser
 from django.contrib.auth.models import Group
