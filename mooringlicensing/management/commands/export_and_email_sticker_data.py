13cfdefac01b1626dbb3c0a2434c354b068ac428
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-06-03 11:12:10 +0800

    Refactor the code for stickers

--- mooringlicensing/management/commands/export_and_email_sticker_data.py
+++ mooringlicensing/management/commands/export_and_email_sticker_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from mooringlicensing.components.main.utils import sticker_export
+from mooringlicensing.components.main.utils import sticker_export, email_stickers_document
 
 
 class Command(BaseCommand):
