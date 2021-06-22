ba9ce7c910f8f1270fcde145666a55cb2ab48eeb
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-06-22 15:51:02 +0800

    Add a cron job for handling reminder email for the mooring licence application

--- mooringlicensing/management/commands/send_waiting_list_application_submit_due_reminder.py
+++ mooringlicensing/management/commands/send_waiting_list_application_submit_due_reminder.py
@@ -1,24 +1,24 @@
 from datetime import timedelta
-from ledger.accounts.models import EmailUser
 
-from django.utils import timezone
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import Q
+from ledger.accounts.models import EmailUser
 
 import logging
 
-from mooringlicensing.components.proposals.email import send_expire_mooring_licence_application_email
+from mooringlicensing.components.proposals.email import send_invitee_reminder_email
 from mooringlicensing.components.main.models import NumberOfDaysType, NumberOfDaysSetting
 from mooringlicensing.components.proposals.models import Proposal, MooringLicenceApplication
-from mooringlicensing.settings import CODE_DAYS_IN_PERIOD_WLA
+from mooringlicensing.settings import CODE_DAYS_IN_PERIOD_WLA, CODE_DAYS_BEFORE_PERIOD_WLA
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'expire mooring licence application if not submitted within configurable number of days after being invited to apply for a mooring licence and send email to inform waiting list allocation holder'
+    help = 'Send email to waiting list allocation holder invited to apply for a mooring licence configurable number of days before end of configurable period in which the mooring licence application needs to be submitted'
 
     def handle(self, *args, **options):
         try:
@@ -31,30 +31,35 @@ class Command(BaseCommand):
         today = timezone.localtime(timezone.now()).date()
 
         # Retrieve the number of days before expiry date of the approvals to email
-        days_type = NumberOfDaysType.objects.get(code=CODE_DAYS_IN_PERIOD_WLA)
-        days_setting = NumberOfDaysSetting.get_setting_by_date(days_type, today)
-        if not days_setting:
-            # No number of days found
-            raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type.name, today))
-        boundary_date = today - timedelta(days=days_setting.number_of_days)
+        days_type_period = NumberOfDaysType.objects.get(code=CODE_DAYS_IN_PERIOD_WLA)
+        days_setting_period = NumberOfDaysSetting.get_setting_by_date(days_type_period, today)
+        if not days_setting_period:
+            raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_period.name, today))
+
+        days_type_before = NumberOfDaysType.objects.get(code=CODE_DAYS_BEFORE_PERIOD_WLA)
+        days_setting_before = NumberOfDaysSetting.get_setting_by_date(days_type_before, today)
+        if not days_setting_before:
+            raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_before.name, today))
+
+        boundary_date = today - timedelta(days=days_setting_period.number_of_days) + timedelta(days=days_setting_before.number_of_days)
 
         logger.info('Running command {}'.format(__name__))
 
         # Construct queries
         queries = Q()
         queries &= Q(processing_status=Proposal.PROCESSING_STATUS_DRAFT)
-        queries &= Q(date_invited__lt=boundary_date)
+        queries &= Q(lodgement_date__lt=boundary_date)
+        queries &= Q(invitee_reminder_sent=False)
 
         for a in MooringLicenceApplication.objects.filter(queries):
             try:
-                a.processing_status = Proposal.PROCESSING_STATUS_EXPIRED
-                a.customer_status = Proposal.CUSTOMER_STATUS_EXPIRED
+                send_invitee_reminder_email(a)
+                a.invitee_reminder_sent = True
                 a.save()
-                send_expire_mooring_licence_application_email(a, MooringLicenceApplication.REASON_FOR_EXPIRY_NOT_SUBMITTED)
-                logger.info('Expired notification sent for Proposal {}'.format(a.lodgement_number))
+                logger.info('Reminder to invitee sent for Proposal {}'.format(a.lodgement_number))
                 updates.append(a.lodgement_number)
             except Exception as e:
-                err_msg = 'Error sending expired notification for Proposal {}'.format(a.lodgement_number)
+                err_msg = 'Error sending reminder to invitee for Proposal {}'.format(a.lodgement_number)
                 logger.error('{}\n{}'.format(err_msg, str(e)))
                 errors.append(err_msg)
 
