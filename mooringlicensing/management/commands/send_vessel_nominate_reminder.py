b5aa6da3be200b9989bdcd4fbe9b44ff9fbb2edb
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-07-15 12:26:43 +0800

    Fix bugs

--- mooringlicensing/management/commands/send_vessel_nominate_reminder.py
+++ mooringlicensing/management/commands/send_vessel_nominate_reminder.py
@@ -106,9 +106,9 @@ class Command(BaseCommand):
 def ml_meet_vessel_requirement(mooring_licence, boundary_date):
     # Return True when mooring_licence has at least one vessel whoose size is more than 6.4m and not sold or sold after the boundary_date
     for proposal in mooring_licence.proposal_set.all():
-        if proposal.vessel_details.vessel.latest_vessel_details.applicable_vessel_length >= 6.4:
+        if proposal.vessel_details and proposal.vessel_details.vessel and proposal.vessel_details.vessel.latest_vessel_details and proposal.vessel_details.vessel.latest_vessel_details.vessel_applicable_length >= 6.4:
             # Vessel meets the length requirements
-            if proposal.vessel_ownership.end_date > boundary_date or proposal.vessel_ownership.end_date is None:
+            if proposal.vessel_ownership.end_date is None or proposal.vessel_ownership.end_date > boundary_date:
                 # Vessel not sold or sold just recently
                 return True
     return False
