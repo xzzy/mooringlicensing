f6b17e3b10e589d3480229111e42c7851b6eb086
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-07-26 12:10:22 +0800

    Add Search Sticker section

--- mooringlicensing/urls.py
+++ mooringlicensing/urls.py
@@ -91,6 +91,7 @@ api_patterns = [
     url(r'^api/mooring_lookup$', proposal_api.GetMooring.as_view(), name='get-mooring'),
     url(r'^api/mooring_lookup_per_bay$', proposal_api.GetMooringPerBay.as_view(), name='get-mooring-per-bay'),
     url(r'^api/vessel_lookup$', proposal_api.GetVessel.as_view(), name='get-vessel'),
+    url(r'^api/sticker_lookup$', approval_api.GetSticker.as_view(), name='get-sticker'),
     url(r'^api/company_names$', proposal_api.GetCompanyNames.as_view(), name='get-company-names'),
     url(r'^api/dcv_vessel_rego_nos$', proposal_api.GetDcvVesselRegoNos.as_view(), name='get-dcv-vessel_rego-nos'),
     url(r'^api/fee_configurations$', payments_api.GetFeeConfigurations.as_view(), name='get-fee-configurations'),
