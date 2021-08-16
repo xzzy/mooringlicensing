c2025c6fe4e0a5a296420be2b3aa388ddc0114e4
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-08-16 11:30:50 +0800

    Update make_docker_image.py

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-git log --pretty=oneline -30 > ./ml_git_history
+cd mooringlicensing/frontend/mooringlicensing/ &&
+npm run build &&
+cd ../../../ &&
+source venv/bin/activate &&
+./manage_ml.py collectstatic --no-input &&
+git log --pretty=oneline -30 > ./ml_git_history &&
 docker image build --no-cache --tag $1 .
