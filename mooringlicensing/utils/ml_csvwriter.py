28ab993928b4b960d993dc486a8bbf0aaf431f4e
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-08-31 19:21:55 +0800

    Further changes to ml_csvwriter

--- mooringlicensing/utils/ml_csvwriter.py
+++ mooringlicensing/utils/ml_csvwriter.py
@@ -4,18 +4,54 @@ import pytz
 import os
 from ledger.settings_base import TIME_ZONE
 from mooringlicensing.settings import BASE_DIR
+from mooringlicensing.components.approvals.models import (
+        Approval, WaitingListAllocation, AnnualAdmissionPermit, 
+        AuthorisedUserPermit, MooringLicence
+        )
 
 
 date_now = datetime.now(pytz.timezone(TIME_ZONE)).date()
-#date_now = datetime.now(pytz.timezone('Australia/Perth')).date()
 date_now_str = date_now.strftime('%Y%m%d')
-print(date_now_str)
-approval_type = 'ml'
-filename = os.path.join(BASE_DIR, 'mooringlicensing', 'utils', 'csv', '{}_{}.csv'.format(approval_type, date_now_str))
-#filename = os.path.join('.', '{}_{}.csv'.format(approval_type, date_now_str))
 
-with open(filename, 'w', newline='') as csvfile:
-    ml_csvwriter = csv.writer(csvfile, delimiter=':', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    ml_csvwriter.writerow(['stuff1', 'stuff2', 'stuff3'])
-    ml_csvwriter.writerow(['stuff4', 'stuff5', 'stuff6'])
+def write_wla():
+    approval_type = WaitingListAllocation.code
+    approvals = WaitingListAllocation.objects.filter(status='current')
+    header_row = []
+    rows = [[wla.lodgement_number] for wla in WaitingListAllocation.objects.filter(status='current')]
+    write_file(approval_type, approvals, header_row, rows)
+
+def write_aap():
+    approval_type = AnnualAdmissionPermit.code
+    approvals = AnnualAdmissionPermit.objects.filter(status='current')
+    header_row = []
+    rows = [[aap.lodgement_number] for aap in AnnualAdmissionPermit.objects.filter(status='current')]
+    write_file(approval_type, approvals, header_row, rows)
+
+def write_aup():
+    approval_type = AuthorisedUserPermit.code
+    approvals = AuthorisedUserPermit.objects.filter(status='current')
+    header_row = []
+    rows = [[aup.lodgement_number] for aup in AuthorisedUserPermit.objects.filter(status='current')]
+    write_file(approval_type, approvals, header_row, rows)
+
+def write_ml():
+    approval_type = MooringLicence.code
+    approvals = MooringLicence.objects.filter(status='current')
+    header_row = []
+    rows = [[ml.lodgement_number] for ml in MooringLicence.objects.filter(status='current')]
+    write_file(approval_type, approvals, header_row, rows)
+
+def write_file(approval_type, approvals, header_row, rows):
+    filename = os.path.join(BASE_DIR, 'mooringlicensing', 'utils', 'csv', '{}_{}.csv'.format(approval_type, date_now_str))
+
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=':', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(header_row)
+        for row in rows:
+            csvwriter.writerow(row)
+
+write_wla()
+write_aap()
+write_aup()
+write_ml()
 
