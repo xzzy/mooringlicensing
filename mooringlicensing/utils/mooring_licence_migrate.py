171f83722d19be1ab7d06d9083c9096eafa0be60
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2021-10-01 11:46:01 +0800

    Added AnnualAdmission Migration script

--- mooringlicensing/utils/mooring_licence_migrate.py
+++ mooringlicensing/utils/mooring_licence_migrate.py
@@ -4,7 +4,7 @@ import json
 import datetime
 from decimal import Decimal
 from django.db import transaction
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from oscar.apps.address.models import Country
 from ledger.accounts.models import EmailUser, Address
 from mooringlicensing.components.proposals.models import (
@@ -219,6 +219,8 @@ class MooringLicenceMigration(object):
 
                     try:
                         vessel_details = VesselDetails.objects.get(vessel=vessel)
+                    except MultipleObjectsReturned:
+                        vessel_details = VesselDetails.objects.filter(vessel=vessel)[0]
                     except ObjectDoesNotExist:
                         vessel_details = VesselDetails.objects.create(
                         vessel_type=vessel_type,
@@ -250,6 +252,7 @@ class MooringLicenceMigration(object):
                         #proposed_issuance_approval={},
                         processing_status='approved',
                         customer_status='approved',
+                        allocated_mooring=mooring,
                         proposed_issuance_approval={
                             "start_date": start_date.strftime('%d/%m/%Y'),
                             "expiry_date": expiry_date.strftime('%d/%m/%Y'),
@@ -307,6 +310,8 @@ class MooringLicenceMigration(object):
 
                     proposal.approval = approval
                     proposal.save()
+                    proposal.allocated_mooring.mooring_licence = approval
+                    proposal.allocated_mooring.save()
 
 #                    moa = MooringOnApproval.objects.create(
 #                        approval=approval,
