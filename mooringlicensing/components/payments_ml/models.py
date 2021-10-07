9d2f96048293d30b5a8db8210899d15347c0de18
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-10-07 12:05:53 +0800

    Refactor fields

--- mooringlicensing/components/payments_ml/models.py
+++ mooringlicensing/components/payments_ml/models.py
@@ -223,9 +223,9 @@ class ApplicationFee(Payment):
     invoice_reference = models.CharField(max_length=50, null=True, blank=True, default='')
     # fee_constructor = models.ForeignKey('FeeConstructor', on_delete=models.PROTECT, blank=True, null=True, related_name='application_fees')
     # fee_item = models.ForeignKey('FeeItem', on_delete=models.PROTECT, blank=True, null=True,)
-    fee_items = models.ManyToManyField('FeeItem', related_name='application_fees')  # For WL/AA/AU/ML
+    # fee_items = models.ManyToManyField('FeeItem', related_name='application_fees')  # For WL/AA/AU/ML
     # fee_items_additional_aa = models.ManyToManyField('FeeItem', related_name='application_fees_additional_aa')  # For additional AA when AU/ML
-    fee_items_for_aa = models.ManyToManyField('FeeItem', related_name='application_fees_for_aa', through='FeeItemApplicationFee')
+    fee_items = models.ManyToManyField('FeeItem', related_name='application_fees', through='FeeItemApplicationFee')
 
     def __str__(self):
         return 'Application {} : Invoice {}'.format(self.proposal, self.invoice_reference)
