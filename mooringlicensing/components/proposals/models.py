be2ce48c40e7f521d5c408db5d66e9f4949a6639
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-05-04 16:44:58 +0800

    Fix a bug where reissued an AUP via the New application and added a mooring, but no new sticker was generated because mooring is added correctly

--- mooringlicensing/components/proposals/models.py
+++ mooringlicensing/components/proposals/models.py
@@ -1255,29 +1255,29 @@ class Proposal(DirtyFieldsMixin, RevisionedMixin):
                     if not self.applicant_address:
                         raise ValidationError('The applicant needs to have set their postal address before approving this proposal.')
 
+                ## update proposed_issuance_approval
+                ria_mooring_name = ''
+                mooring_id = details.get('mooring_id')
+                if mooring_id:
+                    ria_mooring_name = Mooring.objects.get(id=mooring_id).name
+
+                self.proposed_issuance_approval = {
+                    'mooring_bay_id': details.get('mooring_bay_id'),
+                    'mooring_id': mooring_id,
+                    'ria_mooring_name': ria_mooring_name,
+                    'details': details.get('details'),
+                    'cc_email': details.get('cc_email'),
+                    'mooring_on_approval': details.get('mooring_on_approval'),
+                    'vessel_ownership': details.get('vessel_ownership'),
+                }
+                self.save()
+
                 # if no request, must be a system reissue - skip payment section
                 # when reissuing, no new invoices should be created
                 if not request or (request and self.approval and self.approval.reissued):
                     # system reissue or admin reissue
-                    approval, created = self.child_obj.update_or_create_approval(datetime.datetime.now(pytz.timezone(TIME_ZONE)))
+                    approval, created = self.child_obj.update_or_create_approval(datetime.datetime.now(pytz.timezone(TIME_ZONE)), request)
                 else:
-                    ## update proposed_issuance_approval
-                    ria_mooring_name = ''
-                    mooring_id = details.get('mooring_id')
-                    if mooring_id:
-                        ria_mooring_name = Mooring.objects.get(id=mooring_id).name
-
-                    self.proposed_issuance_approval = {
-                        'mooring_bay_id': details.get('mooring_bay_id'),
-                        'mooring_id': mooring_id,
-                        'ria_mooring_name': ria_mooring_name,
-                        'details': details.get('details'),
-                        'cc_email': details.get('cc_email'),
-                        'mooring_on_approval': details.get('mooring_on_approval'),
-                        'vessel_ownership': details.get('vessel_ownership'),
-                    }
-                    self.save()
-
                     ## prepare invoice
                     from mooringlicensing.components.payments_ml.utils import create_fee_lines, make_serializable
                     from mooringlicensing.components.payments_ml.models import FeeConstructor, ApplicationFee
