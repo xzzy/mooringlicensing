b1e888f0c4201b2303c049faeea31c67dc9577be
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-12-16 14:05:52 +0800

    Replace EmailUserAdmin class by one for the segregated system

--- mooringlicensing/admin.py
+++ mooringlicensing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.admin import AdminSite
 from django.contrib import admin
+# from mooringlicensing.components.main import models
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 from django.conf import settings
@@ -11,6 +12,7 @@ from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from copy import deepcopy
 
 
+
 class MooringLicensingAdminSite(AdminSite):
     site_header = 'Mooring Licensing Administration'
     site_title = 'Mooring Licensing Licensing'
@@ -19,31 +21,50 @@ class MooringLicensingAdminSite(AdminSite):
 mooringlicensing_admin_site = MooringLicensingAdminSite(name='mooringlicensingadmin')
 
 # admin.site.unregister(EmailUser) # because this base classAdmin alsready registered in ledger.accounts.admin
+# @admin.register(EmailUser)
+# class EmailUserAdmin(admin.ModelAdmin):
+# # class EmailUserAdmin(ledger_admin.EmailUserAdmin):
+#     """
+#     Overriding the EmailUserAdmin from ledger.accounts.admin, to remove is_superuser checkbox field on Admin page
+#     """
+#
+#     def get_fieldsets(self, request, obj=None):
+#         """ Remove the is_superuser checkbox from the Admin page, if user is MooringLicensingAdmin and NOT superuser """
+#         fieldsets = super(UserAdmin, self).get_fieldsets(request, obj)
+#         #if not obj:
+#         #    return fieldsets
+#
+#         if request.user.is_superuser:
+#             return fieldsets
+#
+#         # User is not a superuser, remove is_superuser checkbox
+#         fieldsets = deepcopy(fieldsets)
+#         for fieldset in fieldsets:
+#             if 'is_superuser' in fieldset[1]['fields']:
+#                 if type(fieldset[1]['fields']) == tuple :
+#                     fieldset[1]['fields'] = list(fieldset[1]['fields'])
+#                 fieldset[1]['fields'].remove('is_superuser')
+#                 break
+#
+#         return fieldsets
+#
+#
 @admin.register(EmailUser)
 class EmailUserAdmin(admin.ModelAdmin):
-# class EmailUserAdmin(ledger_admin.EmailUserAdmin):
-    """
-    Overriding the EmailUserAdmin from ledger.accounts.admin, to remove is_superuser checkbox field on Admin page
-    """
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    ordering = ("email",)
+    search_fields = ("id", "email", "first_name", "last_name")
 
-    def get_fieldsets(self, request, obj=None):
-        """ Remove the is_superuser checkbox from the Admin page, if user is MooringLicensingAdmin and NOT superuser """
-        fieldsets = super(UserAdmin, self).get_fieldsets(request, obj)
-        #if not obj:
-        #    return fieldsets
+    def has_change_permission(self, request, obj=None):
+        if obj is None:  # and obj.status > 1:
+            return True
+        return None
 
-        if request.user.is_superuser:
-            return fieldsets
-
-        # User is not a superuser, remove is_superuser checkbox
-        fieldsets = deepcopy(fieldsets)
-        for fieldset in fieldsets:
-            if 'is_superuser' in fieldset[1]['fields']:
-                if type(fieldset[1]['fields']) == tuple :
-                    fieldset[1]['fields'] = list(fieldset[1]['fields'])
-                fieldset[1]['fields'].remove('is_superuser')
-                break
-
-        return fieldsets
-
-
+    def has_delete_permission(self, request, obj=None):
+        return None
