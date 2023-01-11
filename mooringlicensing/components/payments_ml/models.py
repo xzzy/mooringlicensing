47f10c6c693087265ed6f32482034e70b0c9da3d
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2023-01-11 14:34:11 +0800

    Add uuid field to the ApplicationFee model.  It is used for the payment processes

--- mooringlicensing/components/payments_ml/models.py
+++ mooringlicensing/components/payments_ml/models.py
@@ -206,6 +206,7 @@ class ApplicationFee(Payment):
     invoice_reference = models.CharField(max_length=50, null=True, blank=True, default='')
     fee_items = models.ManyToManyField('FeeItem', related_name='application_fees', through='FeeItemApplicationFee')
     system_invoice = models.BooleanField(default=False)
+    uuid = models.CharField(max_length=36, blank=True, null=True)
 
     def __str__(self):
         return 'Application {} : Invoice {}'.format(self.proposal, self.invoice_reference)
