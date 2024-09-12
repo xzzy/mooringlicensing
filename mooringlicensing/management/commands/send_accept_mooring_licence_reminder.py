9c2c160a69807bea5c194477c83a56264fc911f3
Nishant <phournishant0@gmail.com>
2024-09-12 10:23:58 +0800

    #4924 email reminders before MLA expires (14,7,1 days before)

--- mooringlicensing/management/commands/send_accept_mooring_licence_reminder.py
+++ mooringlicensing/management/commands/send_accept_mooring_licence_reminder.py
@@ -10,7 +10,7 @@ from mooringlicensing.components.approvals.email import send_reminder_to_accept_
 from mooringlicensing.components.approvals.models import Approval, WaitingListAllocation, MooringLicence
 from mooringlicensing.components.main.models import NumberOfDaysType, NumberOfDaysSetting
 from mooringlicensing.management.commands.utils import construct_email_message
-from mooringlicensing.settings import CODE_DAYS_FOR_REMIND_BEFORE_MLA_EXPIRE, CODE_DAYS_FOR_SUBMIT_DOCUMENTS_MLA
+from mooringlicensing.settings import CODE_DAYS_FOR_FIRST_REMINDER_BEFORE_MLA_EXPIRE, CODE_DAYS_FOR_SECOND_REMINDER_BEFORE_MLA_EXPIRE, CODE_DAYS_FOR_FINAL_REMINDER_BEFORE_MLA_EXPIRE, CODE_DAYS_FOR_SUBMIT_DOCUMENTS_MLA
 
 logger = logging.getLogger('cron_tasks')
 cron_email = logging.getLogger('cron_email')
@@ -33,10 +33,20 @@ class Command(BaseCommand):
         #Retrieve the date for which reminder email need to be sent
         if approval_type == WaitingListAllocation.code:
             approval_class = WaitingListAllocation
-            days_type_expire_period = NumberOfDaysType.objects.get(code=CODE_DAYS_FOR_REMIND_BEFORE_MLA_EXPIRE)
-            days_setting_expire_period = NumberOfDaysSetting.get_setting_by_date(days_type_expire_period, today)
-            if not days_setting_expire_period:
-                raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_expire_period.name, today))
+            days_type_expire_period_first = NumberOfDaysType.objects.get(code=CODE_DAYS_FOR_FIRST_REMINDER_BEFORE_MLA_EXPIRE)
+            days_setting_expire_period_first = NumberOfDaysSetting.get_setting_by_date(days_type_expire_period_first, today)
+            if not days_setting_expire_period_first:
+                raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_expire_period_first.name, today))
+            
+            days_type_expire_period_second = NumberOfDaysType.objects.get(code=CODE_DAYS_FOR_SECOND_REMINDER_BEFORE_MLA_EXPIRE)
+            days_setting_expire_period_second = NumberOfDaysSetting.get_setting_by_date(days_type_expire_period_second, today)
+            if not days_setting_expire_period_second:
+                raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_expire_period_second.name, today))
+
+            days_type_expire_period_final = NumberOfDaysType.objects.get(code=CODE_DAYS_FOR_FINAL_REMINDER_BEFORE_MLA_EXPIRE)
+            days_setting_expire_period_final = NumberOfDaysSetting.get_setting_by_date(days_type_expire_period_final, today)
+            if not days_setting_expire_period_final:
+                raise ImproperlyConfigured("NumberOfDays: {} is not defined for the date: {}".format(days_type_expire_period_first.name, today))
 
             days_type_total_period = NumberOfDaysType.objects.get(code=CODE_DAYS_FOR_SUBMIT_DOCUMENTS_MLA)
             days_setting_total_period = NumberOfDaysSetting.get_setting_by_date(days_type_total_period, today)
@@ -45,10 +55,13 @@ class Command(BaseCommand):
         else:
             # Do nothing
             return
-        days_before_expire = days_setting_expire_period.number_of_days
+        days_before_expire_first = days_setting_expire_period_first.number_of_days
+        days_before_expire_second = days_setting_expire_period_second.number_of_days
+        days_before_expire_final = days_setting_expire_period_final.number_of_days
         total_expire_period = days_setting_total_period.number_of_days
-        notification_date = today + timedelta(days=days_before_expire-total_expire_period)  
-        
+        notification_date_first = today + timedelta(days=days_before_expire_first-total_expire_period)
+        notification_date_second = today + timedelta(days=days_before_expire_second-total_expire_period) 
+        notification_date_final = today + timedelta(days=days_before_expire_final-total_expire_period) 
         logger.info('Running command {}'.format(__name__))
 
         # For debug
@@ -61,20 +74,58 @@ class Command(BaseCommand):
             queries = Q()
             queries &= Q(status=Approval.APPROVAL_STATUS_CURRENT)
             queries &= Q(internal_status=Approval.INTERNAL_STATUS_OFFERED)
-            queries &= Q(issue_date__date=notification_date)
-            queries &= Q(expiry_notice_sent=False)
-            # if debug:
-            #     queries = queries | Q(lodgement_number__iexact=approval_lodgement_number)
-            approvals = approval_class.objects.filter(queries)
-        for a in approvals:
+            queries &= Q(issue_date__date=notification_date_first)
+            queries &= Q(expiry_notice_count=0)
+            approval_first_reminder = approval_class.objects.filter(queries)
+            
+            queries = Q()
+            queries &= Q(status=Approval.APPROVAL_STATUS_CURRENT)
+            queries &= Q(internal_status=Approval.INTERNAL_STATUS_OFFERED)
+            queries &= Q(issue_date__date=notification_date_second)
+            queries &= Q(expiry_notice_count=1)
+            approval_second_reminder = approval_class.objects.filter(queries)
+
+            queries = Q()
+            queries &= Q(status=Approval.APPROVAL_STATUS_CURRENT)
+            queries &= Q(internal_status=Approval.INTERNAL_STATUS_OFFERED)
+            queries &= Q(issue_date__date=notification_date_final)
+            queries &= Q(expiry_notice_count=2)
+            approval_final_reminder = approval_class.objects.filter(queries)
+        notice_count = 0                
+        for a in approval_first_reminder:
             try:
-                send_reminder_to_accept_ml_offer_mail(a, days_before_expire, total_expire_period)
-                print(a.expiry_notice_sent)
-                a.expiry_notice_sent = True
+                notice_count = 1
+                #TODO send reminder email based on expiry_notice_count
+                send_reminder_to_accept_ml_offer_mail(a, days_before_expire_first, total_expire_period, notice_count)
+                a.expiry_notice_count+=1
                 a.save()
-                logger.info('Reminder to permission holder sent for Approval {}'.format(a.lodgement_number))
+                logger.info('First Reminder to permission holder sent for Approval {}'.format(a.lodgement_number))
             except Exception as e:
-                err_msg = 'Error sending reminder to permission holder for Approval {}'.format(a.lodgement_number)
+                err_msg = 'Error sending first reminder to permission holder for Approval {}'.format(a.lodgement_number)
+                logger.error('{}\n{}'.format(err_msg, str(e)))
+                errors.append(err_msg)
+        for a in approval_second_reminder:
+            try:
+                #TODO send reminder email based on expiry_notice_count
+                notice_count = 2
+                send_reminder_to_accept_ml_offer_mail(a, days_before_expire_second, total_expire_period, notice_count)
+                a.expiry_notice_count+=1
+                a.save()
+                logger.info('Second Reminder to permission holder sent for Approval {}'.format(a.lodgement_number))
+            except Exception as e:
+                err_msg = 'Error sending second reminder to permission holder for Approval {}'.format(a.lodgement_number)
+                logger.error('{}\n{}'.format(err_msg, str(e)))
+                errors.append(err_msg)
+        for a in approval_final_reminder:
+            try:
+                notice_count = 3
+                #TODO send reminder email based on expiry_notice_count
+                send_reminder_to_accept_ml_offer_mail(a, days_before_expire_final, total_expire_period, notice_count)
+                a.expiry_notice_count+=1
+                a.save()
+                logger.info('Third Reminder to permission holder sent for Approval {}'.format(a.lodgement_number))
+            except Exception as e:
+                err_msg = 'Error sending third reminder to permission holder for Approval {}'.format(a.lodgement_number)
                 logger.error('{}\n{}'.format(err_msg, str(e)))
                 errors.append(err_msg)
 
