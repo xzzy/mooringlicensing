25a64bdcf176fcfa02b2b051047eff2c94447568
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-11-22 01:44:22 +0000

    Added: line col mismatch print out

--- mooringlicensing/utils/export_clean.py
+++ mooringlicensing/utils/export_clean.py
@@ -5,20 +5,17 @@ import re
 
 logger = logging.getLogger(__name__)
 
-def clean(srcpath='/home/jawaidm/Documents/ML_Excel/Lotus_Notes_extracts', outpath='/var/www/mooringlicensing/mooringlicensing/utils/csv/clean'):
+def clean(srcpath='/data/data/projects/mooringlicensing/tmp/ml_export', outpath='/data/data/projects/mooringlicensing/tmp/clean'):
     '''
     from mooringlicensing.utils.export_clean import clean
     clean()
 
     or
 
-    clean(srcpath='/var/www/ml_seg/mooringlicensing/utils/csv/clean_06Feb2024', outpath='/var/www/ml_seg/mooringlicensing/utils/csv/clean')
+    clean(srcpath='/var/www/ml_seg/mooringlicensing/utils/csv/clean_06Feb2024', outpath='/data/data/projects/mooringlicensing/tmp/clean')
     '''
     logger.info(f'srcpath: {srcpath}')
     logger.info(f'outpath: {outpath}')
-
-    # line = '"""aho "" baka"""'
-    # temp = re.sub(r'^"*|"*$', '', line)
 
     isExist = os.path.exists(outpath)
     if not isExist:
@@ -30,24 +27,35 @@ def clean(srcpath='/home/jawaidm/Documents/ML_Excel/Lotus_Notes_extracts', outpa
             files.append(file)
 
     for filename in files:
-        #with open('/home/jawaidm/Documents/ML_Excel/Lotus_Notes_extracts/EmergencyDets20221201-083245.txt', encoding="cp1252") as f:
         with open(srcpath + os.sep + filename, encoding="cp1252") as inf:
             lines = [line.rstrip() for line in inf]
 
+        col_count = 0
         out_filename = filename.split('/')[-1]
         with open(outpath + os.sep + out_filename, 'w', newline='') as outf:
             logger.info(f'Processing file: [{filename}]...')
             wr = csv.writer(outf, quoting=csv.QUOTE_NONE, escapechar='\\') #, delimiter ='|', quotechar = '"')
             line_number = 0
+            header_line = ""
             for line in lines:
+                if line_number == 0:
+                    col_count = line.count('|')
+                    header_line = re.sub(r'^"*|"*$', '', line)
                 line_number += 1  # This is used when an error raised to specify which line raises the error.
                 try:
                     if '|' not in line:
                         # No delimiter in a line, which means no cells in the line.  Skip the process
                         continue
 
-                    # wr.writerow([line.strip('"')])
-                    temp = re.sub(r'^"*|"*$', '', line)  # Remove double quotes at the begginging and end of a string, regardless of their quantity.
+                    temp = re.sub(r'^"*|"*$', '', line)  # Remove double quotes at the beginning and end of a string, regardless of their quantity.
+
+                    line_col_count = temp.count('|')
+                    if line_col_count > col_count:
+                        print("\n___Line",line_number,"has the wrong number of | -", line_col_count, "when it should be",col_count,"___")
+                        print("\n",header_line,"\n")
+                        print(temp, "\n")
+                        print("__________________________________________________\n")
+
                     wr.writerow([temp])
                 except Exception as e:
                     logger.error(f'Error: {e}.  filename: {filename},  line_number: {line_number},  line: {line}')
