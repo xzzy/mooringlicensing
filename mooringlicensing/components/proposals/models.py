94f251f727eb50576a7498e3f0e515f05ffc838b
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2025-03-12 01:40:53 +0000

    Added: missing AA payment calculations for non-renewed MLs

--- mooringlicensing/components/proposals/models.py
+++ mooringlicensing/components/proposals/models.py
@@ -3939,12 +3939,34 @@ class MooringLicenceApplication(Proposal):
             line_items.append(generate_line_item(self.application_type, fee_amount_adjusted, fee_constructor_for_ml, self, current_datetime, self.rego_no))
 
         # For Annual Admission component
-        for vessel_details in vessel_detais_list_to_be_processed:
-            # Annual admission fee is applied to each vessel.
-            if not vessel_details:
-                continue  # When the application was submitted with null-vessel and there are no existing vessels, process reaches this line.
-            vessel_length = vessel_details.vessel_applicable_length
-            max_amount_paid = self.get_max_amount_paid_for_aa_component(target_date, vessel_details.vessel)
+        if vessel_detais_list_to_be_processed:
+            for vessel_details in vessel_detais_list_to_be_processed:
+                # Annual admission fee is applied to each vessel.
+                if not vessel_details:
+                    continue  # When the application was submitted with null-vessel and there are no existing vessels, process reaches this line.
+                vessel_length = vessel_details.vessel_applicable_length
+                max_amount_paid = self.get_max_amount_paid_for_aa_component(target_date, vessel_details.vessel)
+                logger.info(f'Max amount paid so far (for AA component): ${max_amount_paid}')
+                # Check if there is already an AA component paid for this vessel
+                fee_item_for_aa = fee_constructor_for_aa.get_fee_item(vessel_length, self.proposal_type, target_date)
+                logger.info(f'FeeItem (for AA component): [{fee_item_for_aa}] has been retrieved for calculation.')
+                fee_amount_adjusted_additional = self.get_fee_amount_adjusted(fee_item_for_aa, vessel_length, max_amount_paid)
+                logger.info(f'Fee amount adjusted (for AA component): ${fee_amount_adjusted_additional}')
+
+                fee_items_to_store.append({
+                    'fee_item_id': fee_item_for_aa.id,
+                    'vessel_details_id': vessel_details.id if vessel_details else '',
+                    'fee_amount_adjusted': str(fee_amount_adjusted_additional),
+                })
+                line_items.append(generate_line_item(annual_admission_type, fee_amount_adjusted_additional, fee_constructor_for_aa, self, current_datetime, vessel_details.vessel.rego_no))
+        else: #only one vessel to process on the application, not a renewal
+            vessel_details_qs = Vessel.objects.filter(rego_no=self.rego_no).order_by('id')
+            vessel_details = None
+            if vessel_details_qs.exists():
+                vessel_details = vessel_details_qs.last()
+                max_amount_paid = self.get_max_amount_paid_for_aa_component(target_date, vessel_details.vessel)
+            else:
+                max_amount_paid = 0
             logger.info(f'Max amount paid so far (for AA component): ${max_amount_paid}')
             # Check if there is already an AA component paid for this vessel
             fee_item_for_aa = fee_constructor_for_aa.get_fee_item(vessel_length, self.proposal_type, target_date)
