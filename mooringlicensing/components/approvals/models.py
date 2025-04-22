23ea14a4c5415d105d6d72485645cd22f7bd47f7
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2025-04-22 07:57:35 +0000

    Update: fixed stickers not properly updating when a mooring license is surrendered and an AUP has other Moorings

--- mooringlicensing/components/approvals/models.py
+++ mooringlicensing/components/approvals/models.py
@@ -1962,12 +1962,12 @@ class MooringLicence(Approval):
             i.end_date = datetime.date.today()
             i.active = False
             i.save()
-            if i.sticker:
-                if i.sticker.status == Sticker.STICKER_STATUS_CURRENT:
-                    i.sticker.status = Sticker.STICKER_STATUS_TO_BE_RETURNED
-                elif i.sticker.status in [Sticker.STICKER_STATUS_READY, Sticker.STICKER_STATUS_NOT_READY_YET, Sticker.STICKER_STATUS_AWAITING_PRINTING]:
-                    i.sticker.status = Sticker.STICKER_STATUS_CANCELLED                    
-                i.sticker.save()
+            #if i.sticker:
+            #    if i.sticker.status == Sticker.STICKER_STATUS_CURRENT:
+            #        i.sticker.status = Sticker.STICKER_STATUS_TO_BE_RETURNED
+            #    elif i.sticker.status in [Sticker.STICKER_STATUS_READY, Sticker.STICKER_STATUS_NOT_READY_YET, Sticker.STICKER_STATUS_AWAITING_PRINTING]:
+            #        i.sticker.status = Sticker.STICKER_STATUS_CANCELLED                    
+            #    i.sticker.save()
 
         #update aup pdf
         self.generate_au_summary_doc(request.user)
